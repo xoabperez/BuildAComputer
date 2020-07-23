17571fa6df2069e6d9490e6e153f95bb8db74559
Xoab <35434658+xoabperez@users.noreply.github.com>
2020-07-23 17:00:17 -0500

    Finished Fill.asm for project 4 - using assembly, write a program that will blacken the screen if a key is pressed, and clear the screen if no key is pressed.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,72 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	@previousval // initialize to everything white
+	M=0
+
+(KEYCHK) // Continuously check if keyboard has been clicked (value !=0)
+	@KBD
+	D=M
+	@NOCLICK
+	D;JEQ
+	@CLICK
+	0;JMP
+
+(NOCLICK) // If no click, check if it was black before; if so, whiten
+	@previousval
+	D=M
+	@WHITEN
+	D;JNE // if not equal to zero, it was black before
+	@KEYCHK
+	D;JMP
+
+(CLICK) // If click, check if it was white before; if so, blacken
+	@previousval
+	D=M
+	@BLACKEN
+	D;JEQ
+	@KEYCHK
+	D;JMP
+
+(WHITEN) // Set color to 0 and paint with it
+	@color
+	M=0
+	@previousval // previous value will be white
+	M=0
+	@STARTPAINT
+	0;JMP
+
+(BLACKEN) // Set color to -1 and paint with it
+	@color
+	M=-1
+	@previousval // previous value is non-white
+	M=1
+	@STARTPAINT
+	0;JMP
+
+(STARTPAINT) // Set up first register to SCREEN, then loop
+	@SCREEN
+	D=A
+	@screenreg
+	M=D
+	@PAINTLOOP
+	0;JMP
+
+(PAINTLOOP)
+	@KBD
+	D=A
+	@screenreg
+	D=D-M
+	@KEYCHK // Stop once we arrive at the kbd
+	D;JEQ
+	@color
+	D=M
+	@screenreg
+	A=M
+	M=D
+	@screenreg
+	M=M+1
+	@PAINTLOOP
+	0;JMP
+
+	
